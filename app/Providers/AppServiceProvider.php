b5b257e293343b1515dbbfc82254e09e4f95a6f2
Yonathan Nieves <yonathannieves17@gmail.com>
2025-05-29 17:23:50 -0500

    Se ajusta el Provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,24 +4,26 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // if ($this->app->environment('production')) {
-        //     URL::forceScheme('https');
-        // }
+        if ($this->app->environment('production')) {
+            // Laravel confía en los proxies (como Azure)
+            Request::setTrustedProxies(
+                [Request::getClientIp()],
+                Request::HEADER_X_FORWARDED_ALL
+            );
+
+            // Forzar HTTPS
+            URL::forceScheme('https');
+        }
     }
 }
